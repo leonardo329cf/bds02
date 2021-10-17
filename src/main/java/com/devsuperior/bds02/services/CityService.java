0c117cc832b0080acda9b1ff99f1bd70ec5e28eb
leonardo329cf <leonardo329cf@gmail.com>
2021-10-17 20:35:26 -0300

    Implementa delete city: deleteShouldReturnNoContentWhenIndependentId

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,5 +31,9 @@ public class CityService {
 		
 		entity = repository.save(entity);
 		return new CityDTO(entity);		
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 }
